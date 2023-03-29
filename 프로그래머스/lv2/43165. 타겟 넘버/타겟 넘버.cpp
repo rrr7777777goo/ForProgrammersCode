c90fe1baa445bd9cb2085c8f1e3f6c4ae802c6c1
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-03-29 10:48:40 +0900

    [level 2] Title: 타겟 넘버, Time: 0.11 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <vector>
+#include <queue>
 using namespace std;
 
+/*
 vector<int> num;
 int len, tar, ans;
 
@@ -25,4 +27,36 @@ int solution(vector<int> numbers, int target) {
     Add(0, 0);
     
     return ans;
+}
+
+*/
+
+int solution(vector<int> numbers, int target)
+{
+    vector<int> isAlreadyCheck; vector<int> resetVec(2001, 0);
+    queue<int> qu; qu.push(0);
+    
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        isAlreadyCheck = resetVec;
+        
+        queue<int> tqu;
+        
+        while(!qu.empty())
+        {
+            int t = qu.front(); qu.pop();
+            
+            int pl = t + numbers[i], mi = t - numbers[i];
+            if(!isAlreadyCheck[pl + 1000]) { tqu.push(pl); isAlreadyCheck[pl + 1000]; }
+            if(!isAlreadyCheck[mi + 1000]) { tqu.push(mi); isAlreadyCheck[mi + 1000]; }
+        }
+        
+        qu = tqu;
+    }
+    
+    int answer = 0;
+    
+    while(!qu.empty()) { if(qu.front() == target) ++answer; qu.pop(); }
+    
+    return answer;
 }
