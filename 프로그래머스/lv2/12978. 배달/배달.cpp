eba50632133c1bcecde28fc5c7f62cf0e77536b8
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-05 23:43:52 +0900

    [level 2] Title: 배달, Time: 0.04 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int N, vector<vector<int>> road, int K) {
+    
+    vector<vector<int>> graph; vector<int> tV(N+1, 1000000);
+    for(int i = 0; i <= N; i++) { graph.push_back(tV); graph[i][i] = 0; }
+    
+    vector<bool> isCheck(N+1, false);
+    for(int i = 0; i < road.size(); i++)
+    {
+        int x = road[i][0], y = road[i][1], val = road[i][2];
+        if(graph[x][y] > val) { graph[x][y] = val; graph[y][x] = val; }
+    }
+    
+    vector<int> vec_time(N+1, 1000000);
+    for(int i = 1; i <= N; i++) vec_time[i] = graph[1][i];
+    
+    int currentIdx = 1; isCheck[currentIdx] = true;
+    
+    while(true)
+    {
+        int nextIdx = -1, mini = 1000000;
+        
+        for(int i = 1; i <= N; i++)
+        {
+            if(isCheck[i]) continue;
+            
+            if(graph[currentIdx][i] < 1000000)
+            {
+                int c1 = vec_time[currentIdx] + graph[currentIdx][i];
+                int c2 = vec_time[i];
+                vec_time[i] = c1 < c2 ? c1 : c2;
+            }
+            
+            if(vec_time[i] < mini) { nextIdx = i; mini = vec_time[i]; }
+        }
+        
+        if(nextIdx == -1) break;
+        
+        currentIdx = nextIdx; isCheck[currentIdx] = true;
+    }
+    
+    int answer = 0;
+    for(int i = 1; i <= N; i++) if(vec_time[i] <= K) ++answer;
+    return answer;
+}
