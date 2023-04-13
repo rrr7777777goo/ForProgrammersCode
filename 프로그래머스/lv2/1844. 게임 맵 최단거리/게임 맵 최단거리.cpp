f8e0178cba6741288a7edd0a703ea0bab67e7146
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-13 15:39:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.28 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,78 +1,69 @@
 #include<vector>
-#include<queue>
+#include <queue>
 using namespace std;
 
-// [세로][가로]
-int sero, garo;
-bool isBlock[101][101];
+vector<vector<int>> mmm;
 
-int solution(vector<vector<int>> maps)
+int solution(vector<vector<int> > maps)
 {
-    sero = maps.size(); garo = maps[0].size();
+    if(maps[0][0] == 0 || maps[maps.size() - 1][maps[0].size() - 1] == 0) return -1;
+    
+    vector<int> ga(maps[0].size() + 2, 0);
+    
+    for(int i = 0; i < maps.size()+2; i++) mmm.push_back(ga);
+    
     for(int y = 0; y < maps.size(); y++)
     {
         for(int x = 0; x < maps[y].size(); x++)
         {
-            isBlock[y+1][x+1] = maps[y][x];
+            mmm[y+1][x+1] = maps[y][x];
         }
     }
     
-    int count = 1;
+    bool isFind = false;
+    int answer = 1;
     
-    queue<int> qu_sero, qu_garo; qu_sero.push(1); qu_garo.push(1);
+    queue<int> qY, qX; qY.push(1); qX.push(1); 
     
-    bool isFinish = false;
-    while(!qu_sero.empty())
+    while(!qY.empty())
     {
-        ++count;
-        queue<int> tqu_sero, tqu_garo;
-        while(!qu_sero.empty())
-        {
-            int s = qu_sero.front(); qu_sero.pop();
-            int g = qu_garo.front(); qu_garo.pop();
-            
-            if(s >= 2)
-            {
-                if(isBlock[s-1][g])
-                {
-                    if((s-1) == sero && g == garo) { isFinish = true; break; }
-                    tqu_sero.push(s-1); tqu_garo.push(g);
-                    isBlock[s-1][g] = false;
-                }
-            }
-            if(s <= sero-1)
-            {
-                if(isBlock[s+1][g])
-                {
-                    if((s+1) == sero && g == garo) { isFinish = true; break; }
-                    tqu_sero.push(s+1); tqu_garo.push(g);
-                    isBlock[s+1][g] = false;                   
-                }
-            }
-            if(g >= 2)
-            {
-                if(isBlock[s][g-1])
-                {
-                    if(s == sero && (g-1) == garo) { isFinish = true; break; }
-                    tqu_sero.push(s); tqu_garo.push(g-1);
-                    isBlock[s][g-1] = false;
-                }
-            }
-            if(g <= garo-1)
-            {
-                if(isBlock[s][g+1])
-                {
-                    if(s == sero && (g+1) == garo) { isFinish = true; break; }
-                    tqu_sero.push(s); tqu_garo.push(g+1);
-                    isBlock[s][g+1] = false;
-                }
-            }
-        }
+        ++answer;
+        queue<int> tY, tX;
         
-        if(isFinish) break;
-        qu_sero = tqu_sero; qu_garo = tqu_garo;
+        while(!qY.empty())
+        {
+            int y = qY.front(), x = qX.front(); qY.pop(); qX.pop();
+            
+            for(int i = 0; i < 4; i++)
+            {
+                int nY, nX;
+                switch(i)
+                {
+                case 0: nY = y - 1; nX = x; break;
+                case 1: nY = y + 1; nX = x; break;
+                case 2: nY = y; nX = x-1; break;
+                case 3: nY = y; nX = x+1; break;
+                }
+                
+                if(mmm[nY][nX] == 1)
+                {
+                    if(nY == maps.size() && nX == maps[0].size())
+                    {
+                        isFind = true; break;
+                    }
+                    else
+                    {
+                        mmm[nY][nX] = 0;
+                        tY.push(nY); tX.push(nX);
+                    }
+                }
+            }
+            if(isFind) break;
+        }
+        if(isFind) break;
+        qY = tY; qX = tX;
     }
     
-    if(isFinish) return count;
+    if(isFind) return answer;
     else return -1;
 }
