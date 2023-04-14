d9ee46a9253868b61e7ec0555e9b80d7ee82653c
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-14 16:01:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,26 +1,30 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 string solution(string number, int k) {
-    int cnt = 0;
-    
-    for(int i = 0; i < number.size()-1; )
+    string answer = "";
+
+    int biggest = 0;
+    int idx = 0;
+
+    for (int i = 0; i < number.length(); ++i)
     {
-        if(cnt == k) break;
-        
-        if(number[i] < number[i+1])
+        if (biggest < number[i])
         {
-            string newStr001 = number.substr(0, i), newStr002 = number.substr(i+1, -1);
-            number = newStr001; number += newStr002;
-            ++cnt;
-            i = i-1 >= 0 ? i-1 : 0;
+            biggest = number[i];
+            idx = i;
         }
-        else i++;
+
+        if (i == k)
+        {
+            i = idx;
+            ++k;
+            answer.push_back(biggest);
+            biggest = 0;
+        }
     }
-    
-    while(cnt < k) { number.pop_back(); ++cnt; }
-    
-    return number;
+
+    return answer;
 }
