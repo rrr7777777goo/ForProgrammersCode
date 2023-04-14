557abc218090e7449ca170f13e325fa08b49c151
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-14 15:44:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -5,26 +5,22 @@ using namespace std;
 
 string solution(string number, int k) {
     int cnt = 0;
-    while(cnt < k)
+    
+    for(int i = 0; i < number.size()-1; )
     {
-        for(int i = 0; i < number.size(); i++)
+        if(cnt == k) break;
+        
+        if(number[i] < number[i+1])
         {
-            if(i == number.size()-1)
-            {
-                number.pop_back();
-                break;
-            }
-            
-            if(number[i] < number[i+1])
-            {
-                string newStr001 = number.substr(0, i);
-                string newStr002 = number.substr(i+1, -1);
-                number = newStr001;
-                number += newStr002;
-                break;
-            }
+            string newStr001 = number.substr(0, i), newStr002 = number.substr(i+1, -1);
+            number = newStr001; number += newStr002;
+            ++cnt;
+            i = i-1 >= 0 ? i-1 : 0;
         }
-        ++cnt;
+        else i++;
     }
+    
+    while(cnt < k) { number.pop_back(); ++cnt; }
+    
     return number;
 }
