780331f305b80f44ec1c631b03e19b111f143d00
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-21 04:07:58 +0900

    [unrated] Title: 인사고과, Time: 23.14 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
@@ -8,36 +8,30 @@ struct Data001
 {
     int i1, i2, idx, sum;
     Data001(int _i1, int _i2, int dx) {i1 = _i1; i2 = _i2; idx = dx; sum = _i1 + _i2; };
-    
+
     bool operator<(const Data001 &d1) const
     {
         if(i1 == d1.i1) return i2 > d1.i2;
         else return i1 < d1.i1;
     }
-};
 
-struct Data002
-{
-    int idx, sum;
-    
-    Data002(int dx, int su) { idx = dx; sum = su; };
-    
-    bool operator<(const Data002 &d2) const
+    bool operator>(const Data001 &d1) const
     {
-        return sum < d2.sum;
+        return sum < d1.sum;
     }
 };
 
-priority_queue<Data001> qu001;
-priority_queue<Data002> qu002;
-
 int solution(vector<vector<int>> scores) {
 
+
+    priority_queue<Data001> qu001;
     for(int i = 0; i < scores.size(); i++) qu001.push(Data001(scores[i][0], scores[i][1], i));
 
+    priority_queue<Data001, vector<Data001>, greater<Data001>> qu002;
+
     int maxI1 = qu001.top().i1, maxI2 = qu001.top().i2;
-    qu002.push(Data002(qu001.top().idx, qu001.top().sum)); qu001.pop();
-    
+    qu002.push(qu001.top()); qu001.pop();
+
     while(!qu001.empty())
     {
         Data001 chkD1 = qu001.top(); qu001.pop();
@@ -46,20 +40,20 @@ int solution(vector<vector<int>> scores) {
             if(chkD1.idx == 0) { return -1; }
             else continue;
         }
-        else { maxI1 = chkD1.i1; maxI2 = maxI2 >= chkD1.i2 ? maxI2 : chkD1.i2; qu002.push(Data002(chkD1.idx, chkD1.sum)); }
+        else { maxI1 = chkD1.i1; maxI2 = maxI2 >= chkD1.i2 ? maxI2 : chkD1.i2; qu002.push(chkD1); }
     }
-    
+
     int rnk = 1, cnt = 1, maxSum = -1;
-    
+
     while(!qu002.empty())
     {
         int tmpSum = qu002.top().sum, tmpIdx = qu002.top().idx; qu002.pop();
-        
-        if(maxSum == -1) { maxSum = tmpSum; }
-        else { if(maxSum == tmpSum) { ++cnt; } else { maxSum = tmpSum; rnk += cnt; cnt = 1; } }
+
+        if(maxSum != -1) { if(maxSum == tmpSum) { ++cnt; } else { rnk += cnt; cnt = 1; } }
+        maxSum = tmpSum;
         
         if(tmpIdx == 0) { return rnk; }
     }
-    
+
     return -1;
 }
