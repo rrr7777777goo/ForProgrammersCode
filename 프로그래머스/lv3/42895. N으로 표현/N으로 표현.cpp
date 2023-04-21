d579080dc8a8e97884ef3add78e8cfe8b5c9fdca
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-21 23:25:48 +0900

    [level 3] Title: N으로 표현, Time: 2.41 ms, Memory: 3.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -36,12 +36,12 @@ int solution(int N, int number) {
                     if(dp[b][x] != 0) c4 = dp[a][y] / dp[b][x];
                     if(dp[a][y] != 0) c6 = dp[b][x] / dp[a][y];
                     
-                    if(ma[c1] == 0 || ma[c1] > i) { ma[c1] = i; dp[i].push_back(c1); }
-                    if(ma[c2] == 0 || ma[c2] > i) { ma[c2] = i; dp[i].push_back(c2); }
-                    if(ma[c3] == 0 || ma[c3] > i) { ma[c3] = i; dp[i].push_back(c3); }
-                    if(c4 != -199999999) if(ma[c4] == 0 || ma[c4] > i) { ma[c4] = i; dp[i].push_back(c4); }
+                    if(ma[c1] == 0) { ma[c1] = i; dp[i].push_back(c1); }
+                    if(ma[c2] == 0) { ma[c2] = i; dp[i].push_back(c2); }
+                    if(ma[c3] == 0) { ma[c3] = i; dp[i].push_back(c3); }
+                    if(c4 != -199999999) if(ma[c4] == 0) { ma[c4] = i; dp[i].push_back(c4); }
                     if(ma[c5] == 0 || ma[c5] > i) { ma[c5] = i; dp[i].push_back(c5); }
-                    if(c6 != -199999999) if(ma[c6] == 0 || ma[c6] > i) { ma[c6] = i; dp[i].push_back(c6); }
+                    if(c6 != -199999999) if(ma[c6] == 0) { ma[c6] = i; dp[i].push_back(c6); }
                 }
             }
             ++a; --b;
