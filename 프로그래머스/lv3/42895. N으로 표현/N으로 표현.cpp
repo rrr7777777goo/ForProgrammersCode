4f75cf707603ee7dd9da062b6c72f648897366eb
rrr7777777goo <123195016+rrr7777777goo@users.noreply.github.com>
2023-04-21 23:26:47 +0900

    [level 3] Title: N으로 표현, Time: 2.97 ms, Memory: 3.96 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -10,9 +10,9 @@ vector<int> dp[9];
 
 int solution(int N, int number) {
     nN = N; nUmBeR = number;
-    
+
     ma[nN] = 1; dp[1].push_back(nN);
-    
+
     int val = nN, zari = 1; 
     for(int i = 2; i <= 5; i++)
     {
@@ -20,8 +20,8 @@ int solution(int N, int number) {
         val += nN * zari; 
         ma[val] = i; dp[i].push_back(val);
     }
-    
-    
+
+
     for(int i = 2; i <= 8; i++)
     {
         int a = 1, b = i - a;
@@ -35,19 +35,19 @@ int solution(int N, int number) {
                     c3 = dp[a][y] * dp[b][x], c4 = -199999999, c5 = dp[b][x] - dp[a][y], c6 = -199999999;
                     if(dp[b][x] != 0) c4 = dp[a][y] / dp[b][x];
                     if(dp[a][y] != 0) c6 = dp[b][x] / dp[a][y];
-                    
-                    if(ma[c1] == 0) { ma[c1] = i; dp[i].push_back(c1); }
-                    if(ma[c2] == 0) { ma[c2] = i; dp[i].push_back(c2); }
-                    if(ma[c3] == 0) { ma[c3] = i; dp[i].push_back(c3); }
-                    if(c4 != -199999999) if(ma[c4] == 0) { ma[c4] = i; dp[i].push_back(c4); }
+
+                    if(ma[c1] == 0 || ma[c1] > i) { ma[c1] = i; dp[i].push_back(c1); }
+                    if(ma[c2] == 0 || ma[c2] > i) { ma[c2] = i; dp[i].push_back(c2); }
+                    if(ma[c3] == 0 || ma[c3] > i) { ma[c3] = i; dp[i].push_back(c3); }
+                    if(c4 != -199999999) if(ma[c4] == 0 || ma[c4] > i) { ma[c4] = i; dp[i].push_back(c4); }
                     if(ma[c5] == 0 || ma[c5] > i) { ma[c5] = i; dp[i].push_back(c5); }
-                    if(c6 != -199999999) if(ma[c6] == 0) { ma[c6] = i; dp[i].push_back(c6); }
+                    if(c6 != -199999999) if(ma[c6] == 0 || ma[c6] > i) { ma[c6] = i; dp[i].push_back(c6); }
                 }
             }
             ++a; --b;
         }
     }
-    
+
     if(ma[nUmBeR] == 0) return -1;
     else return ma[nUmBeR];
 }
